173288b2332eca17df54298bbc06c594ae84fbb6
unknown <23371323@studentmail.ul.ie>
2023-11-28 14:06:36 +0000

    Maven Tidied

---
+++ TouchPointLtd/src/main/TPMain.java
@@ -0,0 +1,5 @@
+public class TPMain {
+    public static void main(String[] args) {
+        System.out.println("Real Main");
+    }
+}
---
+++ TouchPointLtd/src/test/TPMainTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+public class TPMainTest extends TestCase {
+
+    public void testFunction() {
+
+    }
+}
