81aa4f0d64b3a28d645bc2806d1466eadcc31f0b
Jj Olajitan <145552049+jjola00@users.noreply.github.com>
2023-11-30 15:47:02 +0000

    Comments added

--- TouchPointLtd/src/main/Map.java
+++ TouchPointLtd/src/main/Map.java
@@ -1,8 +1,8 @@
 public class Map {
-    public void getGrid(){
+    public void getGrid(){//method to generate map grid.Use 2D arrays
 
     }
-    public void getPath(){
+    public void getPath(){//starting point is user x,y. Finished is inputted x,y
 
     }
 }
--- TouchPointLtd/src/main/Taxi.java
+++ TouchPointLtd/src/main/Taxi.java
@@ -1,10 +1,10 @@
 public class Taxi {
-    private String name;
-    private float registrationNumber;
-    private int capacity;
+    private String name;//name of taxi driver
+    private float registrationNumber;//individual registration number
+    private int capacity;//diff capacity depending on type of taxi
 
     public Taxi(String name, float registrationNumber, int capacity) {
-        this.name = name;
+        this.name = name;//constructors
         this.registrationNumber = registrationNumber;
         this.capacity = capacity;
     }
@@ -21,7 +21,7 @@ public class Taxi {
         return registrationNumber;
     }
 
-    public void setRegistrationNumber(float regeistrationNumber) {
+    public void setRegistrationNumber(float registrationNumber) {
         this.registrationNumber = registrationNumber;
     }
 
@@ -32,7 +32,7 @@ public class Taxi {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-    public static double getPrice(){
+    public static double getPrice(){//method to calculate price. factors are taxi type, distance and {reviews}{reviews doesn't have to be a system}
         return 0;
     }
 }
