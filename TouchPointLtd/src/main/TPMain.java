1be9b25b6e7d132ff25d4b3b3780845491e7944b
unknown <23371323@studentmail.ul.ie>
2023-11-30 21:57:26 +0000

    Maven functionality test

--- TouchPointLtd/src/main/TPMain.java
+++ TouchPointLtd/src/main/TPMain.java
@@ -3,9 +3,13 @@ public class TPMain {
         TaxiReg taxiReg = new TaxiReg();
         TaxiLarge taxiLarge = new TaxiLarge();
         TaxiPremium taxiPremium = new TaxiPremium();
+
 //        taxiReg.Test(0, 9);//These tests were to see if the values were split right
 //        and the indexes for the different types of vehicles
 //        taxiLarge.Test(10, 16);
 //        taxiPremium.Test(17, 19);
+    }
+    public static int Function(int a, int b){
+        return a + b;
     }
 }
--- TouchPointLtd/src/test/TPMainTest.java
+++ TouchPointLtd/src/test/TPMainTest.java
@@ -3,6 +3,6 @@ import junit.framework.TestCase;
 public class TPMainTest extends TestCase {
 
     public void testFunction() {
-
+assertEquals(3, TPMain.Function(1,2));
     }
 }
