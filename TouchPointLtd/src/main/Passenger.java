bacab3e4ae58b3e34de0aa561691b09b2755f49d
Jj Olajitan <145552049+jjola00@users.noreply.github.com>
2023-12-01 13:36:59 +0000

    Idea for Taxi Premium Class

--- TouchPointLtd/src/main/Passenger.java
+++ TouchPointLtd/src/main/Passenger.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Passenger {
+public class Passenger {
 }
--- TouchPointLtd/src/main/TaxiPremium.java
+++ TouchPointLtd/src/main/TaxiPremium.java
@@ -1,5 +1,35 @@
 public class TaxiPremium extends Taxi{
+    private TaxiPremium premium1;
+    private TaxiPremium premium2;
+    private TaxiPremium premium3;
     public TaxiPremium(String registrationNumber, String carType) {
-        super(registrationNumber, carType, 7);
+        super(registrationNumber, carType, 3);
+        this.premium1 = new TaxiPremium("O 6IJ 7KL","Porsche Panamera");
+        this.premium2 = new TaxiPremium("G 8YZ 9IJ","BMW 7 Series");
+        this.premium3 = new TaxiPremium("L 6UV 7WX","Jaguar XJ");
+    }
+
+    public TaxiPremium getPremium1() {
+        return premium1;
+    }
+
+    public void setPremium1(TaxiPremium premium1) {
+        this.premium1 = premium1;
+    }
+
+    public TaxiPremium getPremium2() {
+        return premium2;
+    }
+
+    public void setPremium2(TaxiPremium premium2) {
+        this.premium2 = premium2;
+    }
+
+    public TaxiPremium getPremium3() {
+        return premium3;
+    }
+
+    public void setPremium3(TaxiPremium premium3) {
+        this.premium3 = premium3;
     }
 }
