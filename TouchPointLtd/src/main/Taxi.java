9eaf5e2640b31e0e658992c1c58f03c8a29345b7
Jj Olajitan <145552049+jjola00@users.noreply.github.com>
2023-12-01 12:23:20 +0000

    Conflicta fixed

--- TouchPointLtd/src/main/Taxi.java
+++ TouchPointLtd/src/main/Taxi.java
@@ -1,5 +1,4 @@
 public class Taxi {
-<<<<<<< HEAD
     private String registrationNumber;//individual registration number
     private String carType;//name of taxi type
     private int capacity;//diff capacity depending on type of taxi
@@ -33,74 +32,4 @@ public class Taxi {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-=======
-        public List<TaxiDrivers> getDrivers(){
-            List<TaxiDrivers> taxiDrivers = new ArrayList<>();
-            String taxiList = "src//main//TaxiDriver.csv";
-        try(BufferedReader br = new BufferedReader(new FileReader(taxiList))){
-            String line;
-            br.readLine();
-            while ((line = br.readLine()) != null){
-                String[] fields = line.split(",");
-                String type = fields[0];
-                String name = fields[1];
-                String registrationNumber = fields[2];
-                String carType = fields[3];
-                int capacity = Integer.parseInt(fields[4]);
-                //taxiDrivers.add(new TaxiDrivers(type, name, registrationNumber, carType, capacity));
-            }
-        }
-            catch(IOException e){
-            e.printStackTrace();
-        }
-        return taxiDrivers;
-        }
-    public static class TaxiDrivers {
-        private String type;//regular, large or premium
-        private String registrationNumber;//individual registration number
-        private String carType;//name of taxi type
-        private int capacity;//diff capacity depending on type of taxi
-
-       /* public TaxiDrivers(String type, String name, String registrationNumber, String carType, int capacity) {
-            this.type = type;
-            this.name = name;
-            this.registrationNumber = registrationNumber;
-            this.carType = carType;
-            this.capacity = capacity;
-        }*/
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getCarType() {
-            return carType;
-        }
-
-        public void setCarType(String carType) {
-            this.carType = carType;
-        }
-
-        public String getRegistrationNumber() {
-            return registrationNumber;
-        }
-
-        public void setRegistrationNumber(String registrationNumber) {
-            this.registrationNumber = registrationNumber;
-        }
-
-        public int getCapacity() {
-            return capacity;
-        }
-
-        public void setCapacity(int capacity) {
-            this.capacity = capacity;
-        }
-    }
-
->>>>>>> ab7df17282f1393bf22a7f4637b918eea58509e4
 }
