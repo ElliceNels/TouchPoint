4696c8c7d8ac54f7fc9a147a8e0361e9bc5f82ba
unknown <23371323@studentmail.ul.ie>
2023-12-01 13:22:43 +0000

    Driver rating added

--- TouchPointLtd/src/main/Driver.java
+++ TouchPointLtd/src/main/Driver.java
@@ -1,10 +1,11 @@
 public class Driver {
     private String name; //name of taxi driver
     private Location currentLocation;
-
-    public Driver(String name, Location currentLocation){
+    private int rating;
+    public Driver(String name, Location currentLocation, int rating){
         this.name = name;
         this.currentLocation = currentLocation;
+        this.rating = rating;
     }
 
     public String getName() {
@@ -21,5 +22,13 @@ public class Driver {
 
     public void setCurrentLocation(Location currentLocation) {
         this.currentLocation = currentLocation;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
