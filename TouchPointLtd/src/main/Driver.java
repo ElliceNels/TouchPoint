f3895e2079ad5fc268317c5aee8c9c6770ad0bc0
unknown <23371323@studentmail.ul.ie>
2023-12-01 13:12:26 +0000

    Driver class completion

--- TouchPointLtd/src/main/Driver.java
+++ TouchPointLtd/src/main/Driver.java
@@ -1,7 +1,25 @@
 public class Driver {
     private String name; //name of taxi driver
+    private Location currentLocation;
 
-    public Driver(){
+    public Driver(String name, Location currentLocation){
+        this.name = name;
+        this.currentLocation = currentLocation;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
     }
 }
