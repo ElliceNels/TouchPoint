a9fe070b5232d0989bafb82558fda709fefbd942
unknown <23371323@studentmail.ul.ie>
2023-12-05 20:19:32 +0000

    Beginning of test 1 implemenation

--- TouchPointLtd/src/test/TPMainTest.java
+++ TouchPointLtd/src/test/TPMainTest.java
@@ -1,6 +1,9 @@
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Random;
+
 public class TPMainTest extends TestCase {
 
     /*@Test
@@ -11,5 +14,25 @@ public class TPMainTest extends TestCase {
     public void testPlaceholder(){
         assertEquals(5, TPMain.placeholder(3, 2));
     }
+    /*public boolean testGetTaxiDrivers(String registrationNumber, Location loc) {
+        List<Taxi> t = Taxi.getTaxiDriver(4);
+        Random rand = new Random();
+        for (int i = 0; i < t.size(); i++) {
+            int startIndex = 88;
+            int endIndex = 197;
+            int randInt = rand.nextInt((endIndex - startIndex + 1)) + startIndex;
+            System.out.println(randInt);
+            int TaxiX = Map.locations[randInt][0];
+            int TaxiY = Map.locations[randInt][1];
+            grid[TaxiX][TaxiY] = new Location(TaxiX, TaxiY);
+            grid[TaxiX][TaxiY].setDisplayTaxi();
+
+        }
+        if (grid[TaxiX][TaxiY] = getDisplayTaxi){
+
+        }
+    }
+
+     */
 
 }
