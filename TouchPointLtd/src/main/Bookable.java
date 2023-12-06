30b13be2e56aa6c3fc544a664a57d93aa7e0e6ae
unknown <23371323@studentmail.ul.ie>
2023-12-06 08:27:41 +0000

    Taxi Interface craeted

---
+++ TouchPointLtd/src/main/Bookable.java
@@ -0,0 +1,12 @@
+public interface Bookable {
+
+    public void RemoveFromMap();
+
+    public void ReturnToMap();
+
+    public void MoveToPassenger();
+
+    public void MoveToDestination();
+
+
+}
