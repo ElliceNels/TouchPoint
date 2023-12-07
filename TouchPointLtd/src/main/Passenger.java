6219b616baa78d0af9f03ae3c0d7bee572e9a337
unknown <23371323@studentmail.ul.ie>
2023-12-07 21:09:01 +0000

    pom updated for CD

--- TouchPointLtd/src/main/Passenger.java
+++ TouchPointLtd/src/main/Passenger.java
@@ -7,19 +7,20 @@ public class Passenger extends User {
     public Passenger(){
         super();
     }
-   /* public findTaxisInRadius(){
+    /* public findTaxisInRadius(){
 
-    }*/
+     }*/
     public void mainMenuRun(User passenger){
 
         System.out.println("Name?");
         passenger.setUsername(in.nextLine());
 
-        System.out.println("Enter your location and destination (0)\nList of map areas (1)");
+        System.out.println("Enter your location and destination to book a taxi (0)\nSee a list of map areas (1)");
         int choice = in.nextInt();
         switch (choice){
             case 0:
                 RegisterPassengerDetails(passenger);
+                ChooseATaxi();
                 break;
             case 1:
                 placeSearch();
@@ -30,66 +31,73 @@ public class Passenger extends User {
         }
     }
 
-   public void RegisterPassengerDetails(User passenger) {
-       boolean validInput = false;
+    public void RegisterPassengerDetails(User passenger) {
+        boolean validInput = false;
 
-       while (!validInput) {
-           try {
-               System.out.println("Enter the x and y coordinates of your current location");
-               int xCoord = in.nextInt();
-               int yCoord = in.nextInt();
-               passenger.setCurrentLocation(new Location(xCoord, yCoord));
+        while (!validInput) {
+            try {
+                System.out.println("Enter the x and y coordinates of your current location");
+                int xCoord = in.nextInt();
+                int yCoord = in.nextInt();
+                passenger.setCurrentLocation(new Location(xCoord, yCoord));
 
-               System.out.println("Enter the x and y coordinates of your destination");
-               int DxCoord = in.nextInt();
-               int DyCoord = in.nextInt();
-               passenger.setDestination(new Location(DxCoord, DyCoord));
-               validInput = true;
-               System.out.println(passenger.getUsername() + " is at " + passenger.getCurrentLocation().getX() + ", " + passenger.getCurrentLocation().getY() + " and wants to go to " + passenger.getDestination().getX() + ", " + passenger.getDestination().getY());
-           } catch (InputMismatchException e) {
-               System.out.println("Invalid input");
-               in.nextLine();
-           }
-       }
-       in.close();
-   }
+                System.out.println("Enter the x and y coordinates of your destination");
+                int DxCoord = in.nextInt();
+                int DyCoord = in.nextInt();
+                passenger.setDestination(new Location(DxCoord, DyCoord));
+                validInput = true;
+                System.out.println(passenger.getUsername() + " is at " + passenger.getCurrentLocation().getX() + ", " + passenger.getCurrentLocation().getY() + " and wants to go to " + passenger.getDestination().getX() + ", " + passenger.getDestination().getY());
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input");
+                in.nextLine();
+            }
+        }
+        in.close();
+    }
 
-   public void placeSearch(){
+    public void placeSearch(){
         Map map = new Map(20, 20);
         boolean doneSearch = false;
-       while(!doneSearch) {
-           in.nextLine();
-           System.out.println("What type of location are you looking for?\nOffices (0), Points of Interest (1), Housing (2), Body of Water (3)\nPress anything else to return\n");
-           String choice = in.nextLine().trim();
-           int count = 1;
-           switch (choice) {
-               case "0":
-                   for (int i = 30; i < 35; i++) {
-                       Location location =  map.mapLocations.get(i);
-                       System.out.print("Office " + count + " at " + location.getX() + ", ");
-                       System.out.print(location.getY());
-                       System.out.println();
-                       count++;
-                   }
-                   System.out.println("Press enter\n");
-                   break;
-               case "1":
-                   System.out.println("blah");
-                   System.out.println("Press enter\n");
-                   break;
-               case "2":
-                   System.out.println("There are 30 houses in the area. Unfortunately I cannot give you their addresses for security reasons.");
-                   System.out.println("Press enter\n");
-                   break;
-               case "3":
-                   System.out.println("There is Blanken Sea at approximately " + map.mapLocations.get(55) + " to " + map.mapLocations.get(61));
-                   System.out.println("Press enter\n");
-                   break;
-               default:
-                   System.out.println("\nReturning");
-                   doneSearch = true;
-                   break;
-           }
-       }
-   }
+        while(!doneSearch) {
+            in.nextLine();
+            System.out.println("What type of location are you looking for?\nOffices (0), Points of Interest (1), Housing (2), Body of Water (3)\nPress anything else to return\n");
+            String choice = in.nextLine().trim();
+            int count = 1;
+            switch (choice) {
+                case "0":
+                    for (int i = 30; i < 35; i++) {
+                        Location location =  map.mapLocations.get(i);
+                        System.out.print("Office " + count + " at " + location.getX() + ", ");
+                        System.out.print(location.getY());
+                        System.out.println();
+                        count++;
+                    }
+                    System.out.println("Press enter\n");
+                    break;
+                case "1":
+                    System.out.println("blah");
+                    System.out.println("Press enter\n");
+                    break;
+                case "2":
+                    System.out.println("There are 30 houses in the area. Unfortunately I cannot give you their addresses for security reasons.");
+                    System.out.println("Press enter\n");
+                    break;
+                case "3":
+                    System.out.println("There is Blanken Sea at approximately " + map.mapLocations.get(55) + " to " + map.mapLocations.get(61));
+                    System.out.println("Press enter\n");
+                    break;
+                default:
+                    System.out.println("\nReturning");
+                    doneSearch = true;
+                    break;
+            }
+        }
+    }
+
+    public void ChooseATaxi(){
+        System.out.println("In a (jj, enter taxi range)KM radius, there are " + "numNearbyTaxis");
+        for (int i = 0; i <= numNearbyTaxis; i++){
+            System.out.println("Taxi " + i + " " + );
+        }
+    }
 }
