247399d7d192d2645f098ba6e139285c5ed0b96b
ElliceNels <23371323@studentmail.ul.ie>
2023-12-18 14:24:58 +0000

    renaming functions for clarity

--- TouchPointLtd/src/main/Map.java
+++ TouchPointLtd/src/main/Map.java
@@ -54,7 +54,7 @@ public class Map {
       return mapLocations;
     }
     public void addToMap(User passenger){
-        storeMapLocations();
+
 
         addToGrid(0, 30);
         addToGrid(30, 35);
@@ -144,7 +144,7 @@ public class Map {
     }
 
 
-    public void choosePlace() { //i is an array element with location
+    public void displayMap() { //i is an array element with location
         for (int i = 0; i < grid.length; i++) {
             System.out.println();
             for (int j = 0; j < grid.length; j++) {
@@ -172,15 +172,16 @@ public class Map {
             }
         } System.out.println();
     }
-    public void Display(User passenger) {
+    public void MapSet(User passenger) {
+        storeMapLocations();
         getLegend();
         addToMap(passenger);
-        choosePlace();
+        displayMap();
     }
     public void DisplayTaxis(User passenger){
         addToMap(passenger);
         getTaxiDrivers(passenger.getPickupPoint());
-        choosePlace();
+        displayMap();
     }
 
     public void getLegend(){
--- TouchPointLtd/src/main/TPMain.java
+++ TouchPointLtd/src/main/TPMain.java
@@ -12,10 +12,10 @@
         Map map = new Map(20, 20); //length and width of the map
         ListSingleton singleton = ListSingleton.getInstance();//calling instance of singleton
         User passenger = singleton.getPassenger();
-        map.Display(passenger);
+        map.MapSet(passenger);
         ((Passenger) passenger).mainMenuRun(passenger, map);        //allows list of taxis to be accessed from singleton
         map.DisplayTaxis(passenger);
         singleton.chooseTaxi().taxiSequence((Passenger) passenger, map);
-        map.Display(passenger);
+        map.MapSet(passenger);
     }
 }
